3968d80e77680ae85977d96f4e9050fac0b2c570
Bo Li <bli25@berkeley.edu>
2016-03-15 20:40:45 -0700

    Fixed a bug in sam_utils.h

--- sam_utils.h
+++ sam_utils.h
@@ -20,7 +20,8 @@
 inline int bam_aux_type2size(char x) {
   if (x == 'C' || x == 'c' || x == 'A') return 1;
   else if (x == 'S' || x == 's') return 2;
-  else if (x == 'I' || x == 'i' || x == 'f' || x == 'F') return 4;
+  else if (x == 'I' || x == 'i' || x == 'f') return 4;
+  else if (x == 'd') return 8;
   else return 0;
 }
 
