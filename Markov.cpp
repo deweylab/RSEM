5f97eb52099b6aeae1746d972caed76d990690d7
Bo Li <libo@broadinstitute.org>
2018-03-05 14:56:00 -0500

    Updated Markov model

--- Markov.cpp
+++ Markov.cpp
@@ -18,10 +18,11 @@
    USA
 */
 
+#include <new>
 #include <cmath>
+#include <limits>
 #include <cstring>
 #include <cassert>
-#include <vector>
 #include <string>
 #include <fstream>
 
@@ -35,97 +36,95 @@ const int Markov::offset[Markov::NSTATES] = {1, 1, 2, 2, 2, 5};
 
 const int Markov::s2pos[Markov::NSTATES] = {-1, 0, -1, 1, -1, 2};
 
-Markov::Markov(bool to_log_space) : to_log_space(to_log_space) {
-	memset(P, 0, sizeof(P));
-	memset(probB, 0, sizeof(probB));
-	ss_P = NULL; ss_probB = NULL;
+Markov::Markov(int mode) : mode(mode), P(NULL), ss_P(NULL), probB(NULL), ss_probB(NULL) {
+	P = new double[NSTATES][NSTATES];
+	probB = new double[3][NSTATES];
+	if (mode == 0) {
+		ss_P = new double[NSTATES][NSTATES];
+		ss_probB = new double[3][NSTATES];
+	}
 }
 
 Markov::~Markov() {
+	if (P != NULL) delete[] P;
+	if (probB != NULL) delete[] probB;
 	if (ss_P != NULL) delete[] ss_P;
 	if (ss_probB != NULL) delete[] ss_probB;
+}
+
+void Markov::clear() {
+	memset(P, 0, sizeof(double) * NSTATES * NSTATES);
+	memset(probB, 0, sizeof(double) * 3 * NSTATES);
 }
 
 void Markov::collect(const Markov* o) {
 	for (int i = 0; i < NSTATES; ++i)
 		for (int j = 0; j < NSTATES; ++j)
 			P[i][j] += o->P[i][j];
-
 	for (int i = 0; i < 3; ++i)
 		for (int j = 0; j < NCODES; ++j)
 			probB[i][j] += o->probB[i][j];
 }
 
 void Markov::finish() {
-	// collect sufficient statistics
-	if (ss_P == NULL) ss_P = new double[NSTATES][NSTATES];
-	memcpy(ss_P, P, sizeof(P));
-	if (ss_probB == NULL) ss_probB = new double[3][NCODES];
-	memcpy(ss_probB, probB, sizeof(probB));
-	// calculate probabilities
+	memcpy(ss_P, P, sizeof(double) * NSTATES * NSTATES);
+	memcpy(ss_probB, probB, sizeof(double) * 3 * NSTATES);
   	ss2p();
-	// convert to log space
-  	if (to_log_space) {
-		for (int i = 0; i < NSTATES; ++i)
-			for (int j = 0; j < NSTATES; ++j)
-				P[i][j] = (P[i][j] == 0.0 ? -1000.0 : log(P[i][j]));
-		for (int i = 0; i < 3; ++i)
-			for (int j = 0; j < NCODES; ++j)
-				probB[i][j] = (probB[i][j] == 0.0 ? -1000.0 : log(probB[i][j]));
-	}
+  	p2logp();
 }
 
-void Markov::clear() {
-	memset(P, 0, sizeof(P));
-	memset(probB, 0, sizeof(probB));
-}
-
-void Markov::read(std::ifstream& fin) {
+void Markov::read(std::ifstream& fin, int choice) {
 	std::string line;
 	int tmp_nstates, tmp_ncodes;
+	double (*in_P)[NSTATES] = NULL, (*in_probB)[NCODES] = NULL;
+
+	switch(choice) {
+		case 0: in_P = P; in_probB = probB; break;
+		case 1: in_P = ss_P; in_probB = ss_probB;
+	}
 
 	assert((fin>> tmp_nstates>> tmp_ncodes) && (tmp_nstates == NSTATES) && (tmp_ncodes == NCODES));
-
 	for (int i = 0; i < NSTATES; ++i)
 		for (int j = 0; j < NSTATES; ++j)
-			assert(fin>> P[i][j]);
-
+			assert(fin>> in_P[i][j]);
 	for (int i = 0; i < 3; ++i)
 		for (int j = 0; j < NCODES; ++j)
-			assert(fin>> probB[i][j]);
-
+			assert(fin>> in_probB[i][j]);
 	getline(fin, line);
+
+	if (mode == 0 && choice == 0) p2logp();
+	if (mode == 2) prepare_for_simulation();
 }
 
-void Markov::write(std::ofstream& fout, bool isProb) {
-	if (to_log_space) ss2p();
+void Markov::write(std::ofstream& fout, int choice) {
+	double (*out_P)[NSTATES] = NULL, (*out_probB)[NCODES] = NULL;
 
-	fout<< "#markov "<< 1 + (NSTATES + 1) + 5<< " Markov model for CIGARs, format: NSTATES NCODES; P; probB"<< std::endl;
+	switch(choice) {
+		case 0: ss2p(); out_P = P; out_probB = probB; break;
+		case 1: out_P = ss_P; out_probB = ss_probB;
+	}
 
+	fout<< "#markov\tMarkov model for CIGARs\t"<< 1 + (NSTATES + 1) + 5<< "\tformat: NSTATES NCODES; P, NSTATES x NSTATES; probB, 3 x NCODES"<< std::endl;
 	fout<< NSTATES<< '\t'<< NCODES<< std::endl;
-  
 	for (int i = 0; i < NSTATES; ++i) {
-		for (int j = 0; j < NSTATES - 1; ++j) fout<< (isProb ? P[i][j] : ss_P[i][j])<< '\t';
-			fout<< (isProb ? P[i][NSTATES - 1] : ss_P[i][NSTATES － 1])<< std::endl;
+		for (int j = 0; j < NSTATES - 1; ++j) fout<< out_P[i][j]<< '\t';
+			fout<< out_P[i][NSTATES － 1]<< std::endl;
 	}
 	fout<< std::endl;
-
 	for (int i = 0; i < 3; ++i) {
-		for (int j = 0; j < NCODES - 1; ++j) fout<< (isProb ? probB[i][j] : ss_probB[i][j])<< '\t';
-		fout<< (isProb ? probB[i][NCODES - 1] : ss_probB[i][NCODES - 1])<< std::endl;
+		for (int j = 0; j < NCODES - 1; ++j) fout<< out_probB[i][j]<< '\t';
+		fout<< out_probB[i][NCODES - 1]<< std::endl;
 	}
 	fout<< std::endl<< std::endl;
 }
 
 void Markov::prepare_for_simulation() {
 	if (P[M][S2] > 0.0 && P[S2][S2] == 0.0) P[S2][S2] = 1.0; // in case S2 only appear as the last base of read/mate
-
 	for (int i = 0; i < NSTATES; ++i) {
 		P[i][0] = P[i][offset[i]];
 		for (int j = 1; j < lens[j]; ++j)
 			P[i][j] = P[i][j - 1] + P[i][offset[i] + j];
 	}
-
 	for (int i = 0; i < 3; ++i)
 		for (int j = 1; j < NCODES; ++j)
 			probB[i][j] += probB[i][j - 1];
@@ -133,16 +132,6 @@ void Markov::prepare_for_simulation() {
 
 void Markov::ss2p() {
 	double sum;
-
-	if (!to_log_space) {
-		for (int i = 0; i < NSTATES; ++i)
-			for (int j = 0; j < NSTATES; ++j)
-				ss_P[i][j] += 0.1; // add a small pseudo count for pre-estimation with unique alignments only
-		for (int i = 0; i < 3; ++i)
-			for (int j = 0; j < NCODES; ++j)
-				ss_probB[i][j] += pseudoC[j];
-	}
-
 	for (int i = 0; i < NSTATES; ++i) {
 		sum = 0.0;
 		for (int j = 0; j < NSTATES; ++j) sum += ss_P[i][j];
@@ -151,7 +140,6 @@ void Markov::ss2p() {
 		else
 			memset(P[i], 0, sizeof(double) * NSTATES);
 	}
-
 	for (int i = 0; i < 3; ++i) {
 		sum = 0.0;
 		for (int j = 0; j < NCODES; ++j) sum += ss_probB[i][j];
@@ -160,4 +148,13 @@ void Markov::ss2p() {
 		else
 			memset(probB[i], 0, sizeof(double) * NCODES);
 	}
+}
+
+void Markov::p2logp() {
+	for (int i = 0; i < NSTATES; ++i)
+		for (int j = 0; j < NSTATES; ++j)
+				P[i][j] = (P[i][j] > 0.0 ? log(P[i][j]) : -std::numeric_limits<double>::infinity());
+	for (int i = 0; i < 3; ++i)
+		for (int j = 0; j < NCODES; ++j)
+			probB[i][j] = (probB[i][j] > 0.0 ? log(probB[i][j]) : -std::numeric_limits<double>::infinity());
 }
--- Markov.hpp
+++ Markov.hpp
@@ -21,35 +21,38 @@
 #ifndef MARKOV_H_
 #define MARKOV_H_
 
-#include <vector>
 #include <fstream>
 
 #include "utils.h"
 #include "sampling.hpp"
 
+// S*M+.*M+S*, require match at both ends
+// this chain runs forever, but we only keep the first N bases
+// parameter estimation is not MLE; but if we assume the probability of ending with I is small, the estimates should be approximately good
+// if simulate a cigar string ends with I or D, restart simulation
 class Markov {
 public:
 	static const int NSTATES = 6;
 	
 	static const int Begin = 0; // Begin -> S1, M
 	static const int S1 = 1; // S1 -> S1, M
-	static const int M = 2; // M -> M, I, D, S2, & End only if M is at the end of the fragment and no S2 observed
-	static const int I = 3; // I -> I, M, D
-	static const int D = 4;	// D -> M, I, D, & End only if D is at the end of the fragment; for simulation, allow D -> S2 if S2 observed
-	static const int S2 = 5;
-	static const int End = 6; // This state is defined but not in P
+	static const int M = 2; // M -> M, I, D, S2
+	static const int I = 3; // I -> M, I, D
+	static const int D = 4;	// D -> M, I, D
+	static const int S2 = 5; // S2 -> S2
 	
 	static char getOpChr(int state) { return state2char[state]; }
 	
-	Markov(bool to_log_space = false);
+	Markov(int mode);
 	~Markov();
 
 	// probabilities are in log space
-	double getLogProb(int curr_state, int prev_state = Begin) const { return P[prev_state][curr_state]; }
+	double getLogProb(int curr_state, int prev_state = Begin) const { 
+		return P[prev_state][curr_state]; 
+	}
 
 	// probabilities in log space
 	double getLogProbBase(int state, int code) const {
-		assert(s2pos[state] >= 0 && code >= 0);
 		return probB[s2pos[state]][code];
 	}
 
@@ -58,21 +61,15 @@ public:
 	}
 
 	void updateBase(int state, int code, double frac) {
-		assert(s2pos[state] >= 0 && code >= 0);
 		probB[s2pos[state]][code] += frac;
 	}
 	
+	void clear();
 	void collect(const Markov* o);
 	void finish();
-	void clear();
 	
-	void read(std::ifstream& fin);
-	void write(std::ofstream& fout, bool isProb = true);
-	
-	void prepare_for_simulation();
-	
-	// only call after prepare_for_simulation is called
-	bool hasS2() const { return P[S2][0] > 0.0; }
+	void read(std::ifstream& fin, int choice);
+	void write(std::ofstream& fout, int choice);
 
 	int simulate(Sampler *sampler, int state = Begin) {
 		return offset[state] + sampler->sample(P[state], lens[state]);
@@ -85,13 +82,16 @@ public:
 private:
 	static const char state2char[NSTATES]; // state to char
 	static const int lens[NSTATES]; // number of transition states for each state
-	static const int offset[NSTATES]; // offsets	
+	static const int offset[NSTATES]; // offsets, pos + offset points to the state	
 	static const int s2pos[NSTATES]; // state to row position in probB
 	
-	double P[NSTATES][NSTATES], (*ss_P)[NSTATES];
-	double probB[3][NCODES], (*ss_probB)[NCODES]; // probability of generating bases given state S1, I/I2, S2
+	int mode; // 0, master; 1, child; 2, simulation
+	double (*P)[NSTATES], (*ss_P)[NSTATES]; // transition matrix
+	double (*probB)[NCODES], (*ss_probB)[NCODES]; // probability of generating bases given state S1, I/I2, S2
 
-	void ss2p();
+	void ss2p(); // from sufficient statistics to probabilities
+	void p2logp(); // convert to log space
+	void prepare_for_simulation();
 };
 
 #endif
