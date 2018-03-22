e91947c763a59c1a1fe40cb9c5410e82cdd725ea
Bo Li <libo@broadinstitute.org>
2018-03-22 00:07:56 -0400

    Added GCbias and PrimerBias

---
+++ GCbias.cpp
@@ -0,0 +1,156 @@
+/* Copyright (c) 2017
+   Bo Li (The Broad Institute of MIT and Harvard)
+   libo@broadinstitute.org
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 3 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+   General Public License for more details.   
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+   USA
+*/
+
+#include <new>
+#include <cstring>
+#include <cassert>
+#include <string>
+#include <fstream>
+
+#include "utils.h"
+#include "FSPD.hpp"
+
+FSPD::FSPD(model_mode_type mode, int number_of_categories, int number_of_bins) : mode(mode), nCat(number_of_categories), nB(number_of_bins) {
+	catUpper = NULL;
+	foreground = background = pmf = cdf = NULL;
+
+	if (mode == INIT) { nCat = nB = 0; return; }
+	catUpper = new int[nCat];
+	create(pmf);
+	create(cdf);
+	if (mode == MASTER || mode == CHILD) {
+		create(foreground);
+		create(background);
+	}
+}
+
+~FSPD::FSPD() {
+	if (catUpper != NULL) delete[] catUpper;
+	if (foreground != NULL) release(foreground);
+	if (background != NULL) release(background);
+	if (pmf != NULL) release(pmf);
+	if (cdf != NULL) release(cdf);
+}
+
+void FSPD::clear() {
+	for (int i = 0; i < nCat; ++i) {
+		memset(foreground[i], 0, sizeof(double) * nB);
+		memset(background[i], 0, sizeof(double) * nB);
+	}
+}
+
+void FSPD::collect(const FSPD* o, bool is_foreground) {
+	for (int i = 0; i < nCat; ++i)
+		for (int j = 0; j < nB; ++j)
+			if (is_foreground) foreground[i][j] += o->foreground[i][j];
+			else background[i][j] += o->background[i][j];
+}
+
+void FSPD::finish() {
+	for (int i = 0; i < nCat; ++i) {
+		for (int j = 0; j < nB; ++j) {
+			pmf[i][j] = (foreground[i][j] + pseudo_count_FSPD) / (background[i][j] + pseudo_count_FSPD);
+			cdf[i][j] = pmf[i][j];
+			if (j > 0) cdf[i][j] += cdf[i][j - 1];
+		}
+		// normalization
+		for (int j = 0; j < nB; ++j) {
+			pmf[i][j] /= cdf[i][nB - 1];
+			cdf[i][j] /= cdf[i][nB - 1];
+		}
+	}
+}
+
+void FSPD::read(std::ifstream& fin, int choice) {
+	std::string line;
+	int tmp_ncat, tmp_nb;
+
+	assert((fin>> line) && (line == "#fspd"));
+	assert(getline(fin, line));
+	assert((fin>> tmp_ncat>> tmp_nb) && (tmp_ncat == nCat) && (tmp_nb == nB));
+	if (nCat == 0) return;
+
+	for (int i = 0; i < nCat; ++i) assert(fin>> catUpper[i]);		
+
+	if (choice == 0) {
+		for (int i = 0; i < nCat; ++i) 
+			for (int j = 0; j < nB; ++i) {
+				assert(fin>> pmf[i][j]);
+				cdf[i][j] = pmf[i][j];
+				if (j > 0) cdf[i][j] += cdf[i][j - 1];
+			}
+	}
+	else {
+		for (int i = 0; i < nCat; ++i)
+			for (int j = 0; j < nB; ++j)
+				assert(fin>> foreground[i][j]);
+		for (int i = 0; i < nCat; ++i)
+			for (int j = 0; j < nB; ++j)
+				assert(fin>> background[i][j]);
+	}
+	assert(getline(fin, line));
+}
+
+void FSPD::write(std::ofstream& fout, int choice) {
+	if (nCat == 0) {
+		fout<< "#fspd\tFragment Start Position Distribution\t3\tformat: nCat nB, nCat == 0 means no FSPD estimation"<< std::endl;
+		fout<< nCat<< "\t"<< nB<< std::endl<< std::endl<< std::endl;
+		return;
+	}
+
+	switch(choice) {
+		case 0:
+			fout<< "#fspd\tFragment Start Position Distribution\t"<< nCat + 5<< "\tformat: nCat nB; nCat values, category upper bound; nCat x nB values for pmf"<< std::endl;
+			fout<< nCat<< '\t'<< nB<< std::endl;
+			for (int i = 0; i < nCat - 1; ++i) fout<< catUpper[i]<< '\t';
+			fout<< catUpper[nCat - 1]<< std::endl<< std::endl;
+			for (int i = 0; i < nCat; ++i) {
+				for (int j = 0; j < nB - 1; ++j) fout<< pmf[i][j]<< '\t';
+				fout<< pmf[i][nB - 1]<< std::endl;
+			}
+			fout<< std::endl<< std::endl;
+			break;
+		case 1:
+			fout<< "#fspd\tFragment Start Position Distribution\t"<< nCat * 2 + 6<< "\tformat: nCat nB; nCat values, category upper bound; nCat x nB values for foreground; nCat x nB values for background"<< std::endl;
+			fout<< nCat<< '\t'<< nB<< std::endl;
+			for (int i = 0; i < nCat - 1; ++i) fout<< catUpper[i]<< '\t';
+			fout<< catUpper[nCat - 1]<< std::endl<< std::endl;
+			for (int i = 0; i < nCat; ++i) {
+				for (int j = 0; j < nB - 1; ++j) fout<< foreground[i][j]<< '\t';
+				fout<< foreground[i][nB - 1]<< std::endl;
+			}
+			fout<< std::endl;
+			for (int i = 0; i < nCat; ++i) {
+				for (int j = 0; j < nB - 1; ++j) fout<< background[i][j]<< '\t';
+				fout<< background[i][nB - 1]<< std::endl;
+			}
+			fout<< std::endl<< std::endl;
+	}
+}
+
+void FSPD::create(double**& arr) {
+	arr = new double*[nCat];
+	for (int i = 0; i < nCat; ++i) arr[i] = new double[nB];
+}
+
+void FSPD::release(double** arr) {
+	for (int i = 0; i < nCat; ++i) delete[] arr[i];
+	delete[] arr;
+}
---
+++ PrimerBias.hpp
@@ -0,0 +1,95 @@
+/* Copyright (c) 2017
+   Bo Li (The Broad Institute of MIT and Harvard)
+   libo@broadinstitute.org
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 3 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+   General Public License for more details.   
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+   USA
+*/
+
+#ifndef PRIMERBIAS_H_
+#define PRIMERBIAS_H_
+
+#include <cassert>
+#include <fstream>
+
+#include "utils.h"
+
+class FSPD {
+public:
+	// if mode == INIT, do not estimate FSPD
+	FSPD(model_mode_type mode, int number_of_categories = 5, int number_of_bins = 10);
+	~FSPD();
+	
+	// efflen = reflen - fragment_length + 1
+	double getProb(int leftmost_pos, int efflen) {
+		if (nCat == 0) return 1.0 / efflen;
+		int cat = locateCategory(efflen);
+		return evalCDF(leftmost_pos + 1, efflen, cat) - evalCDF(leftmost_pos, efflen, cat);
+	}
+
+	void udpate(int leftmost_pos, int efflen, double frac) {
+		assert(foreground != NULL);
+		int cat = locateCategory(efflen);
+
+		int fr, to;
+		double a, b;
+
+		a = double(leftmost_pos) / efflen;
+		fr = leftmost_pos * nB / efflen;
+		to = ((leftmost_pos + 1) * nB - 1) / efflen;
+
+		for (int i = fr; i < to; ++i) {
+			b = double(i) / nB;
+			foreground[i] += (b - a) * efflen * frac;
+			a = b;
+		}
+		b = (leftmost_pos + 1.0) / efflen;
+		foreground[i] += (b - a) * efflen * frac;
+	}
+
+	void clear();
+	void collect(const FSPD* o, bool is_foreground);
+	void finish();
+
+	void read(std::ifstream& fin, int choice); // choice: 0 -> pmf; 1 -> foreground, background
+	void write(std::ofstream& fout, int choice);
+
+private:
+	model_mode_type mode;
+	int nCat, nB; // number of categories and number of bins
+
+	int *catUpper; // catUpper[i] is the maximum length for category i
+	double **foreground, **background, **pmf, **cdf; // pmf = foreground / background; cdf is partial sum of pmf
+
+
+
+	int locateCategory(int efflen) {
+		for (int i = 0; i < nCat; ++i) if (catUpper[i] >= efflen) return i;
+		assert(false);
+	}
+
+	double evalCDF(int leftmost_pos, int efflen, int cat) {
+		double val = double(leftmost_pos) * nB / efflen;
+		int i = int(val - 1e-8); // assume no efflen > 1e7
+	
+		return cdf[cat][i] + (val - i - 1) * pmf[cat][i];
+	}
+
+	void create(double**& arr);
+	void release(double** arr);
+};
+
+
+#endif /* PRIMERBIAS_H_ */
